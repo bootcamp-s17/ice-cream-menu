6d533445c8ddb5d1be59b8904d6b1bc2ca152367
Brandon Spencer <brandon.spencer@gmail.com>
2017-06-30 09:09:45 -0400

    Initial commit and basic layout set up

---
+++ README.md
@@ -0,0 +1,13 @@
+Eyes Cream Ghoulish Dessert Emporium
+
+Setup process:
+1) Read through instructions.md
+2) Consider Humorous Ice Cream shop names
+3)Decide basic layout of page
+	a)using bootstrap cards
+	b)Work up a quick mockup of placeholders
+4) Login to postgres and create database and user
+	- create database iceCreamShop owner postgres;
+	- create user iceCreamMan with password 'helado';
+5) Create Databases
+	
---
+++ app/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EYES CREAM</title>
+	<?php include 'header.php'; ?>
+</head>
+<body>
+	<?php include 'functions.php'; ?>
+	
+	<div class='container col-md-8 row menu-body'>
+		<h1 class='text-center'>Eyes Cream Ghoulish Dessert Emporium</h1>
+		<div class='col col-md-8'>
+			<div class=" card mb-3 ml-3">
+  			<div class='card-header'>
+  				<h4>Seasonal Flavors</h4>
+  			</div>
+				<div class='card-block'>
+					Seasonal Flavors go here
+				
+				</div>
+			</div>
+
+			<div class="card mb-3 ml-3">
+  			<div class='card-header'>
+  				<h4>Regular Flavors</h4>
+  			</div>
+				<div class='card-block'>
+					Regular Flavors go here
+				
+				</div>
+			</div>
+		</div>
+		
+		<div class="col col-md-4">
+			<div class="card mb-3 ml-3">
+  			<div class='card-header'>
+  				<h5>Prices</h5>
+  			</div>
+				<div class='card-block'>
+					Prices go here
+				
+				</div>
+			</div>
+			<div class="card mb-3 ml-3">
+  			<div class='card-header'>
+  				<h5>Toppings</h5>
+  			</div>
+				<div class='card-block'>
+					Toppings go here
+				
+				</div>
+			</div>
+						<div class="card mb-3 ml-3">
+  			<div class='card-header'>
+  				<h5>Serving Options</h5>
+  			</div>
+				<div class='card-block'>
+					Serving Options here
+				
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
